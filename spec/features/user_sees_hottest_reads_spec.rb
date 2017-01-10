04d8cdbd20f87b4299f7cb1c99dfc32a7b504af9
Calaway <calaway@users.noreply.github.com>
2017-01-09 18:58:13 -0700

    Refactor test for clarity.

--- spec/features/user_sees_hottest_reads_spec.rb
+++ spec/features/user_sees_hottest_reads_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "User visits root path", type: :feature do
   context "all reads are from today" do
     scenario "they see the 10 most read links in order of read quantity" do
-      (1...11).each do |index|
+      (1..11).each do |index|
         link = Link.create(url: "http://example#{index}.com")
         index.times do
           link.reads.create
@@ -12,7 +12,7 @@ RSpec.feature "User visits root path", type: :feature do
 
       visit root_path
 
-      expected_content = "Hot Reads! http://example10.com http://example9.com http://example8.com http://example7.com http://example6.com http://example5.com http://example4.com http://example3.com http://example2.com http://example1.com"
+      expected_content = "Hot Reads! http://example11.com http://example10.com http://example9.com http://example8.com http://example7.com http://example6.com http://example5.com http://example4.com http://example3.com http://example2.com"
       expect(page).to have_text(expected_content)
     end
   end
